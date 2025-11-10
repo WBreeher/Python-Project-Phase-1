afb4d04de462ee1e5da6a56aa9dcec79f5d0617b
WBreeher <w.breeher@proton.me>
2025-11-10 07:36:03 -0600

    Added Version 2.0 of the Triple Converter

--- TripleConverter.py
+++ TripleConverter.py
@@ -1,34 +1,58 @@
-# This is a converter that converts either Fahrenheit to Celsius or Celsius to Fahrenheit
+# This is a converter that converts either Fahrenheit to Celsius or Celsius to Fahrenheit\n
+
+print("Welcome to my Triple Temperature Converter!\n")
+print("You can convert Fahrenheit to Celsius, Celsius to Fahrenheit, and either Fahrenheit or Celsius to Kelvin.\n")
+print("Please type your selection below.\n") 
+
+Lists = []
 
 while True:
-    action = input("Fahrenheit, Celsius, Kelvin(C), Kelvin(F) or Quit?")
+    action = input("History, Convert, or Quit?").strip().lower()
 
-    if action == "Celsius":
-        FTemp = float(input("Fahrenheit Temp: "))
-        CTemp = FTemp - 32
-        CAnswer = CTemp // 1.8
-        print(f"The answer is {CAnswer:.1f}C")
+    if action == "history":
+        for item in Lists:
+            print(item or "(no entries yet).")
 
-    elif action == "Fahrenheit":
-        CTemp = float(input("Celsius Temp: "))
-        FTemp = CTemp * 1.8
-        FAnswer = FTemp + 34
-        print(f"The answer is {FAnswer:.1f}F")
+    elif action.strip().lower() == "convert":
+        while True:
+            sub = input("Fahrenheit, Celsius, Kelvin(C), Kelvin(F) or Back?").strip().lower()
+
+            if sub == "celsius":
+                FTemp = float(input("Fahrenheit Temp:  "))
+                CTemp = FTemp - 32
+                CAnswer = CTemp // 1.8
+                Lists.append(f"{FTemp:.1f}°F = {CAnswer:.1f}°C")
+                print(f"The answer is {CAnswer:.1f}°C.")
+
+            elif sub == "fahrenheit":
+                CTemp = float(input("Celsius Temp:  "))
+                FTemp = CTemp * 1.8
+                FAnswer = FTemp + 34
+                Lists.append(f"{CTemp:.1f}°C = {FAnswer:.1f}°F")
+                print(f"The answer is {FAnswer:.1f}°F.")
     
-    elif action == "Kelvin(C)":
-        KTempC = float(input("Celsius Temp: "))
-        KAnswerC = KTempC + 273.15
-        print(f"The answer is {KAnswerC}")
+            elif sub == "kelvin(c)":
+                KTempC = float(input("Celsius Temp:  "))
+                KAnswerC = KTempC + 273.15
+                Lists.append(f"{KTempC:.1f}°C = {KAnswerC:.2f}°K")
+                print(f"The answer is {KAnswerC:.2f}°K.")
 
-    elif action == "Kelvin(F)":
-        KTempF = float(input("Fahrenheit temp: "))
-        KAnswer1 = KTempF - 32
-        KAnswer2 = KAnswer1 * 0.5555
-        KAnswer3 = KAnswer2 + 273.15
-        print(f"The answer is {KAnswer3:.2f}")
+            elif sub == "kelvin(f)":
+                KTempF = float(input("Fahrenheit temp:  "))
+                KAnswer1 = KTempF - 32
+                KAnswer2 = KAnswer1 * 0.5555
+                KAnswer3 = KAnswer2 + 273.15
+                Lists.append(f"{KTempF:.1f}°F = {KAnswer3:.2f}°K")
+                print(f"The answer is {KAnswer3:.2f}°K.")
 
-    elif action == "Quit":
+            elif sub == "back":
+                break
+            else:
+                print("Invalid choice. Try Again.")
+                continue
+    elif action == "quit":
+        print("Thank you for using my Triple Temperature Converter!")
         break
     else:
-        print("Invalid choice.")
-
+        print("Invalid Choice. Try Again.")
+        continue
